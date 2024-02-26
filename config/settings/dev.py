a16dc07618d5cca9f5ba7afaeeb7940b59619117
aniketverma11 <aniketverma1103@gmail.com>
2024-02-26 15:53:41 +0530

    initialize the project

---
+++ config/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-hkt&cn+-hb62lac&lnnk#7bk$633)hv8q!ll4-s0ntozat-04l"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
