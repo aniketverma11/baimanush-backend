eb060b23223a3038ce66a2c215214039f212d684
aniketverma11 <aniketverma1103@gmail.com>
2024-03-08 18:17:36 +0530

    add allow any into category list

--- baimanush_backend/categories/api/v1/views.py
+++ baimanush_backend/categories/api/v1/views.py
@@ -3,9 +3,13 @@ from baimanush_backend.categories.models import Category, SubCategory
 from .serializers import CategorySerializer, SubCategorySerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = []
+    authentication_classes = []
     queryset =  Category.objects.prefetch_related('subcategory_set').all()
     serializer_class = CategorySerializer
 
 class SubCategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = []
+    authentication_classes = []
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
