f0df75c34d2594fed50f66eea1de386604920642
aniketverma11 <aniketverma1103@gmail.com>
2024-03-30 14:14:17 +0530

    create a api that can list articles via

--- baimanush_backend/articles/urls.py
+++ baimanush_backend/articles/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
         "members-only/<str:slug>",
         views.MemberPostDetailViewset.as_view({"get": "member_only_post"}),
     ),
+    path("category-articles/<slug:category_slug>", views.PostListViewset.as_view({"get":"all_article_list_via_category"})),
     path(
         "category-post/<slug:category_slug>",
         views.PostListViewset.as_view({"get": "article_list_via_category"}),
@@ -21,7 +22,6 @@ urlpatterns = [
         "<str:category_slug>/<str:slug>",
         views.PostDetailViewset.as_view({"get": "retrieve"}),
     ),
-    path("category-articles/<slug:category_slug>", views.PostListViewset.as_view({"get":"all_article_list_via_category"})),
     path(
         "members-only/", views.PostListViewset.as_view({"get": "is_member_only_posts"})
     ),
