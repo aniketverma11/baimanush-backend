7d9b7e5975c0a0559a202c6495eec1800e1a9907
aniketverma11 <aniketverma1103@gmail.com>
2024-05-28 18:59:21 +0530

    modify static settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -108,6 +108,16 @@ MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
 COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy' 
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+STATIC_URL = "/static/"
+# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS = [str(APPS_DIR / "static")]
+# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # EMAIL
 # ------------------------------------------------------------------------------
